5e4da7578107703dd0e2c67ac1d5fe86d6c2ae78
Manas Jaiswal <52818738+mjaiswal141@users.noreply.github.com>
2021-10-25 23:13:07 +0530

    Add files via upload

---
+++ Sum-of-Digits.c
@@ -0,0 +1,16 @@
+//Sum-of-Digits
+#include<stdio.h>  
+int main()
+{
+	int n,sum=0,m;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		m=n%10;
+		sum=sum+m;
+		n=n/10;
+	}
+	printf("Sum = %d",sum);
+	return 0;  
+} 
